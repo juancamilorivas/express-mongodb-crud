53aa813c469c130cfba001a6aed1585f156aeb54
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2022-06-14 01:58:34 -0500

    Configuracion del motor de plantillas Handlebars ----------Handlebars Template Engine Configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 import express from "express";
-import indexRouets from './routes/index.routes';
+import exphbs from "express-handlebars";
+import indexRoutes from "./routes/index.routes";
+import path from "path";
 
 const app = express();
 
-app.use(indexRouets)
+app.set("views", path.join(__dirname, "views"));
+
+app.engine(
+  "hbs",
+  exphbs({
+    layoutsDir: path.join(app.get('views'), "layouts"),
+    partialsDir: path.join(app.get('views'), "partials"),
+    defaultlayout: "main",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+
+
+//routes
+app.use(indexRoutes);
 
 export default app;
