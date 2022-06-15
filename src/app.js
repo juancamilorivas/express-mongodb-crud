8e2ab5c8c1499b81162211e80a740a6a17b8fa8a
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2022-06-15 15:34:16 -0500

    conneccion a base de datos mongodb con mongooose completa  //  mongoose connection completed mongosb database

--- src/app.js
+++ src/app.js
@@ -2,23 +2,21 @@ import express from "express";
 import exphbs from "express-handlebars";
 import indexRoutes from "./routes/index.routes";
 import path from "path";
-
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 
 app.engine(
-  "hbs",
+  ".hbs",
   exphbs({
-    layoutsDir: path.join(app.get('views'), "layouts"),
-    partialsDir: path.join(app.get('views'), "partials"),
-    defaultlayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaultLayout: "main",
     extname: ".hbs",
   })
 );
 
-app.set("view engine", "hbs");
-
+app.set("view engine", ".hbs");
 
 //routes
 app.use(indexRoutes);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import app from './app';
+import app from './app'
+import './database'
 
 app.listen(3000);
-
 console.log("server on port", 3000);
