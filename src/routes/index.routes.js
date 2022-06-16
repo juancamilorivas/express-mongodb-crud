807377b7828c6329e5248f4717af7615f270ca43
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2022-06-15 22:11:34 -0500

    Definiendo la manera en que se va a usar el esquema  /////  defining how the eschema will be used

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,19 @@
 import {Router} from 'express';
+import Task from '../models/Task';
 
 const router = Router();
 
 router.get("/", (req, res) => {
     res.render('index');
   });
+
+router.post("/", (req, res) => {})
+router.post('/tasks/add', (req, res) => {
+    const task = Task(req.body)
+    console.log(task);
+  res.send('saved')
+})
+
 
   router.get("/about", (req, res) => {
     res.render('about');
