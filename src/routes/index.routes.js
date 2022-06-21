612684221d4ad84838647d858ab73fb39f4ef667
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2022-06-21 17:23:16 -0500

    Todo ok hasta minuto 1:27:42 video Fazt web.com

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,27 +1,27 @@
-import {Router} from 'express';
-import Task from '../models/Task';
+import { Router } from "express";
+import Task from "../models/Task";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.render('index');
-  });
+router.get("/", async (req, res) => {
+  const tasks = await Task.find().lean();
+  res.render("index", { tasks: tasks });
+});
 
-router.post("/", (req, res) => {})
-router.post('/tasks/add', (req, res) => {
-    const task = Task(req.body)
-    console.log(task);
-  res.send('saved')
-})
+router.post("/tasks/add", async (req, res) => {
+  const task = Task(req.body);
 
+  await task.save();
 
-  router.get("/about", (req, res) => {
-    res.render('about');
-  });
+  res.redirect("/");
+});
 
-  router.get("/edit", (req, res) => {
-    res.render('edit');
-  });
-  
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
-  export default router;
+router.get("/edit", (req, res) => {
+  res.render("edit");
+});
+
+export default router;
