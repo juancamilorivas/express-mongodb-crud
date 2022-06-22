0e276fc7798843ed321fd73aef099c36bddc554c
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2022-06-21 21:22:45 -0500

    todo bien hasta minuto 2:05:37

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -34,9 +34,23 @@ router.get("/edit/:id", async (req, res) => {
 });
 
 router.post("/edit/:id", async (req, res) => {
-  const {id} = req.params
-  await Task.findByIdAndUpdate(id, req.body)
-  res.redirect("/")
+  const { id } = req.params;
+  await Task.findByIdAndUpdate(id, req.body);
+  res.redirect("/");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
+router.get("/toggleDone/:id", async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findById(id);
+  task.done = !task.done;
+  await task.save();
+  res.redirect("/");
 });
 
 export default router;
