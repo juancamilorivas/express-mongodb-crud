db858e2fb956dece2af501a0375f60dcb5e7c3ef
juancamilorivas <67404147+juancamilorivas@users.noreply.github.com>
2022-06-22 00:30:24 -0500

    Todo bien hasta minuto 2:32:45

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+### Environment variables
+
+MONGODB_URI = mongodb://localhost:27017/********
+
+
+Para clonar este repositorio debes crear un archivo llamado .env donde se deben crer las variables de entorno que se mencionan arriba "MONGODB_URI"
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,56 +1,22 @@
 import { Router } from "express";
-import Task from "../models/Task";
+
+//importacion de "controllers"
+import {
+  renderTasks,
+  createTask,
+  renderTaskEdit,
+  editTask,
+  deleteTask,
+  taskToggleDone,
+} from "../controllers/tasks.controller";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find().lean();
-  res.render("index", { tasks: tasks });
-});
-
-router.post("/tasks/add", async (req, res) => {
-  try {
-    const task = Task(req.body);
-
-    await task.save();
-
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/about", (req, res) => {
-  res.render("about");
-});
-
-router.get("/edit/:id", async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id).lean();
-    res.render("edit", { task });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-
-router.post("/edit/:id", async (req, res) => {
-  const { id } = req.params;
-  await Task.findByIdAndUpdate(id, req.body);
-  res.redirect("/");
-});
-
-router.get("/delete/:id", async (req, res) => {
-  const { id } = req.params;
-  await Task.findByIdAndDelete(id);
-  res.redirect("/");
-});
-
-router.get("/toggleDone/:id", async (req, res) => {
-  const { id } = req.params;
-  const task = await Task.findById(id);
-  task.done = !task.done;
-  await task.save();
-  res.redirect("/");
-});
+router.get("/", renderTasks);
+router.post("/tasks/add", createTask);
+router.get("/tasks/:id/toggleDone", taskToggleDone);
+router.get("/tasks/:id/edit", renderTaskEdit);
+router.post("/tasks/:id/edit", editTask);
+router.get("/tasks/:id/delete", deleteTask);
 
 export default router;
